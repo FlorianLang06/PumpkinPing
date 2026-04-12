fe103fb8fc4bce932c4f6a26836b42b07e06d4c9
Florian Lang <144521337+FlorianLang06@users.noreply.github.com>
2026-04-12 18:27:13 +0200

    View ping of other players (#2)
    * fix: moved color selection to separate function
    
    * feat: added support to see ping of other players
    
    * fix: moved colors to Component API
    
    * feat: implemented permissions check for ping of others
    
    * feat: show help message for console
    
    * feat: updated readme
    
    * chore: new version
    
    * chore: new version

--- README.md
+++ README.md
@@ -3,15 +3,23 @@
 > [!NOTE]
 > After joining it will take a while to show the real ping. We use the ping method from Pumpkin so we can't influence this.
 
-**PumpkinPing** is a simple ping plugin for [Pumpkin](https://github.com/Pumpkin-MC/Pumpkin) which shows a player whats his ping to the server is.
+**PumpkinPing** is a simple ping plugin for [Pumpkin](https://github.com/Pumpkin-MC/Pumpkin) that adds a `/ping` command to check your own ping or query the ping of other players.
 
 ## Usage
 A player just needs to execute `/ping` to see this own ping.
 
+A op-player can execute `/ping <playername>` to get the ping of other players.
+
 ## Permission
-The permissions for this command is `PumpkinPing:ping` which is allowed by default.
+The permissions to view own ping is `PumpkinPing:command.ping` which is allowed by default.
+
+The permissions to view ping of other players is `PumpkinPing:command.ping.other` which is allowed by default for op level 4.
 
 ## Showcase
 <img width="183" height="24" alt="image" src="https://github.com/user-attachments/assets/e5a2b554-53d4-4da7-bec9-4c4b575ec620" />
 <img width="186" height="23" alt="image" src="https://github.com/user-attachments/assets/b028f580-d22b-4492-9ed8-857a6ab7cf59" />
 <img width="194" height="21" alt="image" src="https://github.com/user-attachments/assets/982de81e-a5eb-4771-bbb3-17249fb1179c" />
+
+## Coming soon
+
+- Translations
--- src/commands/ping_command.rs
+++ src/commands/ping_command.rs
@@ -1,29 +1,62 @@
-use pumpkin_plugin_api::command::{Command, CommandError, CommandSender, ConsumedArgs};
+use pumpkin_plugin_api::command::{Command, CommandError, CommandNode, CommandSender, ConsumedArgs};
+use pumpkin_plugin_api::command_wit::{Arg, ArgumentType};
 use pumpkin_plugin_api::commands::CommandHandler;
+use pumpkin_plugin_api::permission::{Permission, PermissionDefault, PermissionLevel};
+use pumpkin_plugin_api::text::{NamedColor, TextComponent};
 use pumpkin_plugin_api::{Context, Server};
-use pumpkin_plugin_api::permission::{Permission, PermissionDefault};
-use pumpkin_plugin_api::text::TextComponent;
+
+const PERMISSION_PING: &str = "PumpkinPing:command.ping";
+const PERMISSION_PING_OTHER: &str = "PumpkinPing:command.ping.other";
+const PLAYER_ARGUMENT: &str = "player";
 
 struct PingCommandExecutor;
 impl CommandHandler for PingCommandExecutor {
-    fn handle(&self, sender: CommandSender, _server: Server, _args: ConsumedArgs) -> pumpkin_plugin_api::Result<i32, CommandError> {
+    fn handle(&self, sender: CommandSender, server: Server, args: ConsumedArgs) -> pumpkin_plugin_api::Result<i32, CommandError> {
+        if let Arg::Players(players) = args.get_value(PLAYER_ARGUMENT) {
+            if !sender.has_permission(&server, PERMISSION_PING_OTHER) {
+                return Err(CommandError::PermissionDenied)
+            }
+
+            for player in players {
+                let ping = player.get_ping();
+
+                let msg = player.get_display_name();
+                msg.color_named(NamedColor::Green);
+
+                let msg_second_part = TextComponent::text(" has a ping of ");
+                msg_second_part.color_named(NamedColor::Gray);
+                msg.add_child(msg_second_part);
+
+                msg.add_child(get_ping_msg_part(ping));
+
+                sender.send_message(msg);
+            }
+
+            return Ok(1);
+        }
+
         match sender.as_player() {
             Some(player) => {
                 let ping = player.get_ping();
 
-                let color = if ping < 30 {
-                    "a"
-                } else if ping < 80 {
-                    "e"
-                } else {
-                    "c"
-                };
+                let msg = TextComponent::text("Your ping is ");
+                msg.color_named(NamedColor::Gray);
 
-                let component = TextComponent::text(&format!("§7Your ping is §{}{}ms", color, ping));
-                sender.send_message(component);
+                msg.add_child(get_ping_msg_part(ping));
+
+                sender.send_message(msg);
             }
             None => {
-                sender.send_message(TextComponent::text("You are not a player!"))
+                let msg = TextComponent::text("You are not a player!");
+                msg.color_named(NamedColor::Red);
+
+                if sender.has_permission(&server, PERMISSION_PING_OTHER) {
+                    let help_message = TextComponent::text(" You can use /ping <Playername> to see the ping of a player.");
+                    help_message.color_named(NamedColor::Red);
+                    msg.add_child(help_message);
+                }
+
+                sender.send_message(msg)
             }
         };
 
@@ -31,17 +64,38 @@ impl CommandHandler for PingCommandExecutor {
     }
 }
 
+fn get_ping_msg_part(ping: u32) -> TextComponent {
+    let ping_part = TextComponent::text(format!("{}ms", ping).as_str());
+    ping_part.color_named(get_color(ping));
+    ping_part
+}
+
+fn get_color(ping: u32) -> NamedColor {
+    if ping < 30 {
+        NamedColor::Green
+    } else if ping < 80 {
+        NamedColor::Yellow
+    } else {
+        NamedColor::Red
+    }
+}
+
 pub fn register_command(context: Context) -> pumpkin_plugin_api::Result<()> {
-    let permission = "PumpkinPing:ping";
+    context.register_permission(&Permission {
+        node: PERMISSION_PING_OTHER.to_string(),
+        description: "Allows to show ping of other players".to_string(),
+        default: PermissionDefault::Op(PermissionLevel::Four),
+        children: Vec::new(),
+    })?;
 
     context.register_permission(&Permission {
-        node: permission.to_string(),
+        node: PERMISSION_PING.to_string(),
         description: "Allows to show own ping".to_string(),
         default: PermissionDefault::Allow,
         children: Vec::new(),
     })?;
 
-    context.register_command(init_command_tree(), permission);
+    context.register_command(init_command_tree(), PERMISSION_PING);
 
     Ok(())
 }
@@ -50,5 +104,11 @@ fn init_command_tree() -> Command {
     let names = ["ping".to_string()];
     let description = "Show the ping of a player";
 
-    Command::new(&names, description).execute(PingCommandExecutor)
+    let command = Command::new(&names, description);
+
+    command.then(
+        CommandNode::argument(PLAYER_ARGUMENT, &ArgumentType::Players).execute(PingCommandExecutor)
+    );
+
+    command.execute(PingCommandExecutor)
 }
